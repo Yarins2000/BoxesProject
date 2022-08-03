560435b214df1a1c897b5ebe42129e92ce6a3d1a
yarin sabag <yarinsabag2@gmail.com>
2022-08-03 22:10:19 +0300

    After DeleteNode function check

--- BoxesProject/Program.cs
+++ BoxesProject/Program.cs
@@ -6,14 +6,25 @@ namespace BoxesProject
     {
         static void Main(string[] args)
         {
-            BinarySearchTree<int, string> t = new BinarySearchTree<int, string>();
-            t.AddNode(4, "four");
-            t.AddNode(5, "five");
-            t.AddNode(1, "one");
-            t.AddNode(10, "ten");
-            t.AddNode(20, "twenty");
-            Console.WriteLine(t);
-
+            var tree = new BinarySearchTree<double, string>();
+            tree.AddNode(5, null);
+            tree.AddNode(6, null);
+            tree.AddNode(4, null);
+            tree.AddNode(8, null);
+            tree.AddNode(10, null);
+            tree.AddNode(3, null);
+            tree.AddNode(4.5, null);
+            tree.AddNode(4.3, null);
+            tree.AddNode(4.7, null);
+            tree.AddNode(4.8, null);
+            tree.AddNode(4.6, null);
+            tree.AddNode(4.2, null);
+            tree.AddNode(4.65, null);
+            tree.TraverseInOrder();
+            Console.WriteLine();
+            Console.WriteLine("====================");
+            tree.RemoveNode(4.5);
+            tree.TraverseInOrder();
         }
     }
 }
--- Models/BinarySearchTree.cs
+++ Models/BinarySearchTree.cs
@@ -1,6 +1,6 @@
 ﻿namespace Models
 {
-    public class BinarySearchTree<K, V> where K:IComparable<K>
+    public class BinarySearchTree<K, V> where K : IComparable<K>
     {
         internal TreeNode<K, V>? Root { get; set; }
 
@@ -19,7 +19,7 @@
         {
             if (data.CompareTo(root.Data) <= 0)
             {
-                if(root.Left == null)
+                if (root.Left == null)
                     root.Left = new TreeNode<K, V>(data, value);
                 else
                     AddNode(data, value, root.Left);
@@ -33,31 +33,35 @@
             }
         }
 
-        public void TraverseInOrder()
-        {
-            TraverseInOrder(Root);
-        }
-        private void TraverseInOrder(TreeNode<K, V>? root)
-        {
-            if (root != null)
-            {
-                TraverseInOrder(root.Left);
-                Console.Write(root.Data + " ");
-                TraverseInOrder(root.Right);
-            }
-        }
-
-        public V GetValue(K key)
+        private TreeNode<K, V> Get(K key)
         {
             if (Root == null)
-                return default(V);
+                return null;
+            return Get(key, Root);
+        }
+        private TreeNode<K, V> Get(K key, TreeNode<K, V> root)
+        {
+            if (root == null)
+                return null;
+            if (root.Data.Equals(key))
+                return root;
+
+            else if (key.CompareTo(root.Data) <= 0)
+                return Get(key, root.Left);
+            else
+                return Get(key, root.Right);
+        }
+
+        private V GetValue(K key)
+        {
+            if (Root == null)
+                return default;
             return GetValue(key, Root);
         }
         private V GetValue(K key, TreeNode<K, V> root)
         {
-            if (Root == null)
-                return default(V);
-
+            if (root == null)
+                return default;
             if (root.Data.Equals(key))
                 return root.Value;
 
@@ -67,21 +71,122 @@
                 return GetValue(key, root.Right);
         }
 
-        public override string ToString()
+        private void RemoveNodeWithoutChildren(TreeNode<K, V> deleteNode)
         {
-            return ToString(Root);
+            if (deleteNode.Left == null && deleteNode.Right == null)
+            {
+                var parent = GetPerent(deleteNode.Data);
+                if (parent.Right == deleteNode)
+                    parent.Right = null;
+                else
+                    parent.Left = null;
+            }
         }
-        private string ToString(TreeNode<K, V>? root)
+        private void RemoveNodeWithOneChild(TreeNode<K, V> deleteNode)
         {
-            string res = "";
+            var perent = GetPerent(deleteNode.Data);
+            if (perent != null)
+            {
+                if (deleteNode.Left != null && deleteNode.Right == null)
+                {
+                    if (perent.Right == deleteNode)   // if parent's RIGHT is the node we want to remove
+                        perent.Right = deleteNode.Left;  // parent's RIGHT becomes the deleteNode LEFT
+                    else
+                        perent.Left = deleteNode.Left; // else parent's LEFT becomes the deleteNode LEFT
+                    return;
+                }
+
+                else if (deleteNode.Left == null && deleteNode.Right != null)
+                {
+                    if (perent.Right == deleteNode)  // if parent's LEFT is the node we want to remove
+                        perent.Right = deleteNode.Right; // parent's RIGHT becomes the deleteNode RIGHT
+                    else
+                        perent.Left = deleteNode.Right; // else parent's LEFT becomes the deleteNode RIGHT
+                }
+            }
+        }
+        private void RemoveNodeWithTwoChildren(TreeNode<K, V> deleteNode)
+        {
+            if (deleteNode.Left != null && deleteNode.Right != null)
+            {
+                var minimum = GetMinimumNode(deleteNode.Right);
+                var parant = GetPerent(deleteNode.Data);
+
+                var minParent = GetPerent(minimum.Data);  //find minimum's parent
+                if (minParent != deleteNode)
+                {
+                    minParent.Left = null;
+                    if (minimum.Right != null) // if minimum has a right node
+                    {
+                        minParent.Left = minimum.Right; // minimum's parent takes the node of the minimum's RIGHT node
+                    }
+                }
+                minimum.Left = deleteNode.Left;    // minimum takes the RIGHT and the LEFT of the deleteNode
+                if(deleteNode.Right != minimum)
+                    minimum.Right = deleteNode.Right;
+                
+
+                if (parant.Right == deleteNode)   // connecting the node's parent with the minimum
+                    parant.Right = minimum;
+                else
+                    parant.Left = minimum;
+            }
+        }
+
+        public void RemoveNode(K data)
+        {
+            var deleteNode = Get(data);
+            if(deleteNode != null && deleteNode != Root)
+            {
+                RemoveNodeWithoutChildren(deleteNode);
+                RemoveNodeWithOneChild(deleteNode);
+                RemoveNodeWithTwoChildren(deleteNode);
+                deleteNode = null;
+            }
+        }
+
+        private TreeNode<K, V> GetMinimumNode(TreeNode<K, V> t)
+        {
+            if (t.Left == null)
+                return t;
+            return GetMinimumNode(t.Left);
+        }
+
+        // Get the parent of the given value
+        private TreeNode<K, V> GetPerent(K data)
+        {
+            if (Root != null)
+                return GetPerent(data, Root);
+            return null;
+        }
+        private TreeNode<K, V> GetPerent(K data, TreeNode<K, V> t)
+        {
+            if (t == null)
+                return null;
+
+            if (t.Left != null && t.Left.Data.Equals(data))
+                return t;
+            else if (t.Right != null && t.Right.Data.Equals(data))
+                return t;
+            else if (data.CompareTo(t.Data) < 0)
+                return GetPerent(data, t.Left);
+            else
+                return GetPerent(data, t.Right);
+        }
+
+        public void TraverseInOrder()
+        {
+            TraverseInOrder(Root);
+        }
+        private void TraverseInOrder(TreeNode<K, V>? root)
+        {
             if (root != null)
             {
                 TraverseInOrder(root.Left);
-                //Console.Write(root.Data + " ");
-                res += $"Data: {root.Data}, Value: {root.Value}\n";
+                Console.Write(root.Data + ", ");
                 TraverseInOrder(root.Right);
             }
-            return res;
         }
+
     }
 }
